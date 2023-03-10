aa6ec0cb7b5c5ce93e3d54254cb880c66ad5e804
samirasingh19 <samira.singh021@gmail.com>
2023-03-10 21:54:59 +0530

    Employeewage

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Day3EmployeeWage;
+
+namespace EmployeeWageComputationProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+            EmployeeAttendence.Employeeattendence();
+        }
+    }
+}
